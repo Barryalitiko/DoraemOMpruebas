d13df7869a13b358d073126512449aeb190f3f91
Krampus B. <tiobarryx@gmail.com>
2024-11-27 14:39:06 +0100

    Actualizar pasado.js

--- src/commands/admin/pasado.js
+++ src/commands/admin/pasado.js
@@ -1,14 +1,16 @@
-const { getLastDeletedMessages } = require("../../utils/database"); // Asegúrate de importar la función
 
-// Definir el comando
-exports.lastdeleted = {
+const { PREFIX } = require("../../config");
+const { getLastDeletedMessages } = require("../../utils/database");
+
+module.exports = {
   name: "lastdeleted",
   description: "Muestra los últimos 6 mensajes borrados en el grupo.",
+  usage: `${PREFIX}lastdeleted`,
+  commands: ["lastdeleted"],
   async handle({ remoteJid, sendReply }) {
     try {
       // Obtener los últimos 6 mensajes borrados
       const deletedMessages = getLastDeletedMessages(remoteJid);
-
       if (!deletedMessages || deletedMessages.length === 0) {
         await sendReply("No se encontraron mensajes borrados recientes.");
         return;
@@ -16,16 +18,14 @@ exports.lastdeleted = {
 
       // Formatear los mensajes para mostrarlos
       const formattedMessages = deletedMessages
-        .map(
-          (msg, idx) =>
-            `@${msg.userId.split("@")[0]}:\n*Mensaje ${idx + 1}:* ${msg.messageText}`
-        )
+        .map((msg, idx) => {
+          const userId = msg.userId.split("@")[0];
+          return `@${userId}:\n*Mensaje ${idx + 1}:* ${msg.messageText}`;
+        })
         .join("\n\n");
 
       // Enviar la respuesta con los mensajes
-      await sendReply(
-        `Estos son los últimos mensajes borrados:\n\n${formattedMessages}`
-      );
+      await sendReply(`Estos son los últimos mensajes borrados:\n\n${formattedMessages}`);
     } catch (error) {
       console.error("Error al obtener los mensajes borrados:", error);
       await sendReply("Ocurrió un error al intentar recuperar los mensajes borrados.");
