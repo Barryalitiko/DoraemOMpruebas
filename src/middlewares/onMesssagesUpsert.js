0e0fff9ee333d8ffa154e832f7d6af32b2c7903e
Krampus B. <tiobarryx@gmail.com>
2024-11-27 19:55:23 +0100

    Actualizar onMesssagesUpsert.js

--- src/middlewares/onMesssagesUpsert.js
+++ src/middlewares/onMesssagesUpsert.js
@@ -2,29 +2,29 @@ const { dynamicCommand } = require("../utils/dynamicCommand");
 const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
 const { handleAntiLongText } = require("../middlewares/antiLongText");
 const { addDeletedMessage } = require("../utils/database");
+const { antifloodMiddleware } = require("../middlewares/antiflood");
 
 exports.onMessagesUpsert = async ({ socket, messages }) => {
   if (!messages.length) {
     return;
   }
-
   for (const webMessage of messages) {
     const commonFunctions = loadCommonFunctions({ socket, webMessage });
-
     if (!commonFunctions) {
       continue;
     }
-
+    await antifloodMiddleware(commonFunctions, webMessage);
     if (webMessage.message && webMessage.message.delete) {
       const groupId = webMessage.key.remoteJid;
       const userId = webMessage.key.participant || webMessage.key.remoteJid;
       const messageText = webMessage.message?.conversation || webMessage.message?.extendedTextMessage?.text;
-
       if (messageText) {
         addDeletedMessage(groupId, userId, messageText);
       }
     }
-
     await dynamicCommand(commonFunctions);
   }
 };
+
+
+
