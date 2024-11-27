29920539daa19818b546fd9f30dd25a627ab95ea
Krampus B. <tiobarryx@gmail.com>
2024-11-27 19:43:14 +0100

    Actualizar antiflood.js

---
+++ src/middlewares/antiflood.js
@@ -0,0 +1,27 @@
+const { isActiveAntiFloodGroup } = require('../database');
+
+const antifloodMiddleware = async (message, next) => {
+  const groupId = (link unavailable);
+  const isAntiFloodEnabled = await isActiveAntiFloodGroup(groupId);
+
+  if (isAntiFloodEnabled) {
+    const floodThreshold = 5; // ajusta este valor según tus necesidades
+    const recentMessages = await message.chat.getRecentMessages(10);
+
+    const isFlood = recentMessages.filter((msg) => msg.from === message.from).length >= floodThreshold;
+
+    if (isFlood) {
+      await message.delete();
+      return;
+    }
+  }
+
+  next();
+};
+
+module.exports = antifloodMiddleware;
+
+
+
+
+
