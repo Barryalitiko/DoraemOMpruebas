2e35dda0783415388c794b80a531adf64c78407d
Krampus B. <tiobarryx@gmail.com>
2024-11-29 11:53:36 +0100

    Actualizar onMesssagesUpsert.js

--- src/middlewares/onMesssagesUpsert.js
+++ src/middlewares/onMesssagesUpsert.js
@@ -3,15 +3,21 @@ const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
 const { handleAntiLongText } = require("../middlewares/antiLongText");
 const { addDeletedMessage } = require("../utils/database");
 const { getMensajesRecientes, addMensajeReciente } = require("./antiflood");
+const { handleAntiFlood } = require("../middlewares/antiFlood"); // Asegúrate de tener la función de antiflood importada
 
-const antifloodMiddleware = async (commonFunctions, webMessage) => {
+const antifloodMiddleware = async (commonFunctions, webMessage, socket) => {
   const groupId = webMessage.key.remoteJid;
   const userId = webMessage.key.participant || webMessage.key.remoteJid;
   const mensaje = webMessage.message;
 
+  // Verificar si el grupo tiene antiflood activado
+  if (!isActiveAntiFloodGroup(groupId)) {
+    return;
+  }
+
   const mensajesRecientes = await getMensajesRecientes(groupId, userId);
   const maxMensajes = 5; // Máximo de mensajes permitidos en 1 minuto
-  const tiempoEspera = 60000; // Tiempo de espera en milisegundos (1 minuto)
+  const tiempoEspera = 60000; // 1 minuto en milisegundos
 
   if (mensajesRecientes.length >= maxMensajes) {
     // El usuario ha enviado demasiados mensajes, eliminar el mensaje actual
@@ -19,6 +25,7 @@ const antifloodMiddleware = async (commonFunctions, webMessage) => {
     return;
   }
 
+  // Añadir el mensaje reciente a la base de datos
   await addMensajeReciente(groupId, userId, mensaje);
 };
 
@@ -33,7 +40,8 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
       continue;
     }
 
-    await antifloodMiddleware(commonFunctions, webMessage);
+    // Middleware para antiflood
+    await antifloodMiddleware(commonFunctions, webMessage, socket);
 
     if (webMessage.message && webMessage.message.delete) {
       const groupId = webMessage.key.remoteJid;
@@ -47,4 +55,3 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
     await dynamicCommand(commonFunctions);
   }
 };
-
